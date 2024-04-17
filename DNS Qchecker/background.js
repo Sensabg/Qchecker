fc2b549bcc877257b1109b7fcf89705c4ede21eb
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-04-18 02:31:34 +0300

    Update background.js

--- DNS Qchecker/background.js
+++ DNS Qchecker/background.js
@@ -3,11 +3,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var recordType = request.recordType;
     var domain = request.domain;
 
-    // Perform DNS lookup using an external DNS service
     fetch(`https://dns.google/resolve?name=${domain}&type=${recordType}`)
       .then(response => response.json())
       .then(result => {
-        // Extract only the "Answer" section from the DNS response
         var answerSection = result.Answer;
         sendResponse(answerSection);
       })
@@ -15,10 +13,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         console.error("Error fetching DNS information:", error);
         sendResponse(null);
       });
+    return true;
 
-    return true;  // To indicate that sendResponse will be called asynchronously
   } else if (request.action === "openInNewWindow") {
-    // Open popup.html in a new window
     chrome.windows.create({
       url: 'popup.html',
       type: 'popup',
